2a7e2f13a4148a0e83389d4891c65e17b7987dd7
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-06-10 00:58:58 +0300

    Add files via upload

---
+++ C++/Problem6.h
@@ -0,0 +1,34 @@
+//
+//  Problem6.h
+//  ProjectEuler
+//
+//  Created by Furkan Erturk on 10/06/2021.
+//  Copyright © 2021 Nuh Furkan Erturk. All rights reserved.
+//
+
+#ifndef Problem6_h
+#define Problem6_h
+
+int squares(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n*n;
+        n--;
+    }
+    return sum;
+}
+
+int sumSquares(int n) {
+    int sum = 0;
+    while (n > 0) {
+        sum += n;
+        n--;
+    }
+    return sum*sum;
+}
+
+int sumSquareDifferece(int n) {
+    return sumSquares(n) - squares(n);
+}
+
+#endif /* Problem6_h */
