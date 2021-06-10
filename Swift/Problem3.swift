22fdcdcab6cbb59ca8232c666693b2e59dbeb87f
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-06-10 21:47:04 +0300

    Add files via upload

--- Swift/Problem3.swift
+++ Swift/Problem3.swift
@@ -7,37 +7,46 @@
 //
 
 import Foundation
-var primes: [Int] = []
 
-func findPrime(num: Int) -> Int {
-    var x = 2
-    while x <= num {
-        if num%x == 0 {
-            primes.append(x)
-            findPrime(num: num/x)
-            break
-        } else {
-            x+=1
+class FindPrimes {
+    var primes: [Int] = []
+    
+    func findPrime(num: Int) -> Int {
+        var x = 2
+        while x <= num {
+            if num%x == 0 {
+                primes.append(x)
+                findPrime(num: num/x)
+                break
+            } else {
+                x+=1
+            }
         }
+        return returnMaxPrime()
     }
-    return returnMaxPrime()
-}
-
-func displayPrimes() -> Void {
-    for n in primes {
-        print(n)
-    }
-}
-
-func returnMaxPrime() -> Int {
-    var max = 1
-    for n in primes {
-        if n > max {
-            max = n
+    
+    func displayPrimes() -> [Int] {
+        for n in primes {
+            print(n)
         }
+        return primes
     }
-    return max
+
+    func returnMaxPrime() -> Int {
+        var max = 1
+        for n in primes {
+            if n > max {
+                max = n
+            }
+        }
+        return max
+    }
+    
 }
+
+
+
+
 
 // call function findPrime(num: *input number*)
 // where input number is the parameter
