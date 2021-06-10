60dbd336857f689b3364e1785877e477267b3b5c
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-06-10 11:41:16 +0300

    Update Problem1.swift

--- Swift/Problem1.swift
+++ Swift/Problem1.swift
@@ -23,3 +23,4 @@ func sumTwoMultiples(max: Int, num1: Int, num2:Int) -> Int {
 }
 
 // call sumTwoMultiples with (max Bound, number 1, number 2) as parameters
+// in our case -> sumTwoMultiples(1000, 3, 5)
