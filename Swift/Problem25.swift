6500fa50e209c48db0700554cc7650d1c1b47dff
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-06-16 10:32:48 +0300

    Add files via upload

---
+++ Swift/Problem25.swift
@@ -0,0 +1,26 @@
+//
+//  Problem25.swift
+//  ProjectEulerSwift
+//
+//  Created by Furkan Erturk on 16/06/2021.
+//  Copyright © 2021 Nuh Furkan Erturk. All rights reserved.
+//
+
+import Foundation
+
+
+// ATTENTION - BigNumber.swift form the repo Data-Structures-and-Algortihms was used in this problem
+func firstIndexWithThousand() -> Int {
+    var first = BigNumber(newNumber: "1")
+    var sec = BigNumber(newNumber: "1")
+    var n = 2
+    
+    while first.myNumber.count < 1000 {
+        let temp:BigNumber = first
+        first = first + sec
+        sec = temp
+        n += 1
+    }
+    return n
+}
+// call function firstIndexWithThousand()
